6be0fae91592f077dfa7a01873b9768bbae8ee0b
HOT-DOG <rightl2053@gmail.com>
2025-03-12 04:37:21 +0500

    middleware added

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,38 @@
-//import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-//import { DASHBOARD_PAGES } from './config/pages-url.config'
-//import { EnumTokens } from './services/auth-token.service'
+import { DASHBOARD_PAGES } from './config/pages-url.config'
+import { EnumTokens } from './services/auth-token.service'
 
-//export async function middleware(request: NextRequest) {
-//    const { url, cookies, nextUrl } = request
+export async function middleware(request: NextRequest) {
+    const { url, cookies, nextUrl } = request
 
-//    const refreshToken = cookies?.get(EnumTokens.REFRESH_TOKEN)?.value
-//    console.log(refreshToken, cookies.get(EnumTokens.REFRESH_TOKEN), cookies.get(EnumTokens?.REFRESH_TOKEN))
-//    const isAuthPage = url.includes('/auth')
+    const refreshToken = cookies?.get(EnumTokens.REFRESH_TOKEN)?.value
+    console.log(refreshToken, cookies.get(EnumTokens.REFRESH_TOKEN), cookies.get(EnumTokens?.REFRESH_TOKEN))
+    const isAuthPage = url.includes('/auth')
 
-//    // Если пользователь заходит на "/", отправляем его в "/listify"
-//    if (nextUrl.pathname === '/') {
-//        return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, request.url))
-//    }
+    // Если пользователь заходит на "/", отправляем его в "/listify"
+    if (nextUrl.pathname === '/') {
+        return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, request.url))
+    }
 
-//    // Если пользователь уже авторизован, но пытается зайти на страницу авторизации — редиректим в Dashboard
-//    if (isAuthPage && refreshToken) {
-//        return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
-//    }
+    // Если пользователь уже авторизован, но пытается зайти на страницу авторизации — редиректим в Dashboard
+    if (isAuthPage && refreshToken) {
+        return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
+    }
 
-//    // Разрешаем доступ к auth-страницам
-//    if (isAuthPage) {
-//        return NextResponse.next()
-//    }
+    // Разрешаем доступ к auth-страницам
+    if (isAuthPage) {
+        return NextResponse.next()
+    }
 
-//    // Если нет refreshToken, отправляем на страницу логина
-//    if (!refreshToken) {
-//        return NextResponse.redirect(new URL('/auth/login', request.url))
-//    }
+    // Если нет refreshToken, отправляем на страницу логина
+    if (!refreshToken) {
+        return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
 
-//    return NextResponse.next()
-//}
+    return NextResponse.next()
+}
 
-//export const config = {
-//    matcher: ['/', '/listify/:path*', '/auth/:path*']
-//}
+export const config = {
+    matcher: ['/', '/listify/:path*', '/auth/:path*']
+}
